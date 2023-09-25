7c0666c6735aa3bba257e99f01e880c0a0115f6c
Jeff Bothel <jeffbothel@scidomum.cloud>
2023-09-24 23:44:26 -0600

    Committing Module 1 Critical Thinking My cocde that I wrote both in Java and Psuedocode for this assignment

---
+++ CSC320_CTA1_Option1/CSC320_CTA1_Option1.java
@@ -0,0 +1,13 @@
+public class CSC320_CTA1_Option1 {
+    public static void main(String[] args) {
+        String givenName = "John";
+        String familyName = "Doe";
+        String streetAddress = "123 Main Street";
+        String city = "Anytown";
+        String state = "NY";
+        String zipCode = "12345";
+        System.out.println(givenName + " " + familyName);
+        System.out.println(streetAddress);
+        System.out.println(city + ", " + state + " " + zipCode);
+    }
+}
